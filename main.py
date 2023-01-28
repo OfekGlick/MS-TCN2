Implemented class-weighting via median frequency balancing and fixed graph naming issues. Also increased batch size and learning rate and configured other hyperparameters

main.py
@@ -8,6 +8,7 @@ import argparse
 import random
 from os import listdir
 from os.path import isfile, join
+import numpy as np
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 seed = 1538574472
@@ -22,16 +23,16 @@ parser.add_argument('--dataset', default="gtea")
 parser.add_argument('--split', default='1')
 
 parser.add_argument('--features_dim', default='1280', type=int)
-parser.add_argument('--bz', default='1', type=int)
-parser.add_argument('--lr', default='0.0005', type=float)
+parser.add_argument('--bz', default='5', type=int)
+parser.add_argument('--lr', default='0.0025', type=float)
 
-parser.add_argument('--num_f_maps', default='64', type=int)
+parser.add_argument('--num_f_maps', default='65', type=int)
 
 # Need input
-parser.add_argument('--num_epochs', type=int)
-parser.add_argument('--num_layers_PG', type=int)
-parser.add_argument('--num_layers_R', type=int)
-parser.add_argument('--num_R', type=int)
+parser.add_argument('--num_epochs', default=30, type=int)
+parser.add_argument('--num_layers_PG', default=10, type=int)
+parser.add_argument('--num_layers_R', default=10, type=int)
+parser.add_argument('--num_R', default=3, type=int)
 
 args = parser.parse_args()
 
@@ -77,11 +78,31 @@ try:
 except Exception:
     latest = 0
 new_exp = f"exp{latest + 1}"
-
+print(f"Experiment: {new_exp}")
 results_dir = "./results/" + str(new_exp) + "/{}/{}/test"
 
 features_path = "/datashare/APAS/features/"
 
+
+def calc_class_weights(labels_path):
+    class_labels_files = [f for f in listdir(labels_path) if isfile(join(labels_path, f))]
+    possible_labels = [f"G{i}" for i in range(6)]
+    class_counts = {k: 0 for k in possible_labels}
+    sample_count = 0
+    for label_file in class_labels_files:
+        with open(os.path.join(labels_path, label_file), 'r') as file:
+            content = [(line.split()[-1], int(line.split()[1]) - int(line.split()[0]) - 1) for line in file.readlines()]
+            sample_count += sum([amount for _, amount in content])
+            for class_label, amount in content:
+                class_counts[class_label] += amount
+
+    class_counts = {k: v / sample_count for k, v in class_counts.items()}
+    median_freq = np.median(list(class_counts.values()))
+    class_weights = sorted([(k, median_freq / v) for k, v in class_counts.items()], key=lambda x: x[0])
+    return torch.tensor([weight for _, weight in class_weights])
+
+
+class_weights = calc_class_weights(gt_path).float().to(device)
 if not os.path.exists(model_dir):
     os.makedirs(model_dir)
 # if not os.path.exists(results_dir):
@@ -101,28 +122,35 @@ clogger = task.get_logger()
 
 print("Starting training!")
 if args.action == "train":
-    for weighted_flag in [True, False]:
-        for val_path_fold, test_path_fold, features_path_fold in fold_files:
-            fold_num = features_path_fold.split("/")[-2]
-            print(f"\t{fold_num}")
-            fold_results_dir_reg = results_dir.format(fold_num, "Regular")
-            fold_results_dir_Weighted = results_dir.format(fold_num, "Weighted")
-            vid_list_file, vid_list_file_val, vid_list_file_test = fold_split(features_path_fold, val_path_fold,
-                                                                              test_path_fold)
-            batch_gen_train = BatchGenerator(num_classes, actions_dict, gt_path, features_path_fold, sample_rate)
+    for kl_flag in [False, True]:
+        for label_class_weights in [True, False]:
+            for weighted_flag in [True, False]:
+                for val_path_fold, test_path_fold, features_path_fold in fold_files:
+                    fold_num = features_path_fold.split("/")[-2]
+                    print(f"\t{fold_num}")
+                    folder_weight_flag = "Weighted" if weighted_flag else "Regular"
+                    folder_kl_flag = "KL" if kl_flag else "No_KL"
+                    folder_class_labels_flag = "ClassWeighted" if kl_flag else "NotClassWeighted"
+                    folder_name = results_dir.format(fold_num,
+                                                     folder_weight_flag + "_" + folder_kl_flag + "_" + folder_class_labels_flag)
+                    vid_list_file, vid_list_file_val, vid_list_file_test = fold_split(features_path_fold, val_path_fold,
+                                                                                      test_path_fold)
+                    batch_gen_train = BatchGenerator(num_classes, actions_dict, gt_path, features_path_fold,
+                                                     sample_rate)
 
-            batch_gen_train.read_data(vid_list_file)
-            batch_gen_val = BatchGenerator(num_classes, actions_dict, gt_path, features_path_fold, sample_rate)
+                    batch_gen_train.read_data(vid_list_file)
+                    batch_gen_val = BatchGenerator(num_classes, actions_dict, gt_path, features_path_fold, sample_rate)
 
-            batch_gen_val.read_data(vid_list_file_val)
-            # Regular
-            trainer = Trainer(num_layers_PG, num_layers_R, num_R, num_f_maps, features_dim, num_classes, fold_num,
-                              fold_num, weighted=weighted_flag)
-            trainer.train(model_dir, batch_gen_train, batch_gen_val, num_epochs=num_epochs, batch_size=bz,
-                          learning_rate=lr, device=device, clogger=clogger)
-            try:
-                os.makedirs(fold_results_dir_Weighted if weighted_flag else fold_results_dir_reg)
-            except:
-                pass
-            trainer.predict(model_dir, fold_results_dir_Weighted if weighted_flag else fold_results_dir_reg,
-                            features_path_fold, vid_list_file_test, num_epochs, actions_dict, device, sample_rate)
+                    batch_gen_val.read_data(vid_list_file_val)
+                    trainer = Trainer(num_layers_PG, num_layers_R, num_R, num_f_maps, features_dim, num_classes,
+                                      fold_num, fold_num, weighted=weighted_flag, kl=kl_flag,
+                                      class_weights=class_weights if label_class_weights else None)
+                    trainer.train(model_dir, batch_gen_train, batch_gen_val, num_epochs=num_epochs, batch_size=bz,
+                                  learning_rate=lr, device=device, clogger=clogger)
+                    try:
+                        os.makedirs(folder_name)
+                    except:
+                        pass
+                    trainer.predict(model_dir, folder_name,
+                                    features_path_fold, vid_list_file_test, num_epochs, actions_dict, device,
+                                    sample_rate)

model.py
@@ -20,7 +20,7 @@ class MS_TCN2(nn.Module):
         #   Added by us
         self.weighted = weighted
         self.Ws = nn.ParameterList(
-            [copy.deepcopy(nn.Parameter(data=torch.rand(1), requires_grad=True)) for s in range(num_R)])
+            [copy.deepcopy(nn.Parameter(data=(torch.rand(1)), requires_grad=True)) for s in range(num_R)])
 
     def forward(self, x):
         out = self.PG(x)
@@ -132,14 +132,22 @@ class DilatedResidualLayer(nn.Module):
 
 class Trainer:
     def __init__(self, num_layers_PG, num_layers_R, num_R, num_f_maps, dim, num_classes, dataset, split,
-                 weighted=False):
+                 weighted=False, kl=False, class_weights=None):
         self.model = MS_TCN2(num_layers_PG, num_layers_R, num_R, num_f_maps, dim, num_classes, weighted)
-        self.ce = nn.CrossEntropyLoss(ignore_index=-100)
+        if class_weights is not None:
+            self.ce = nn.CrossEntropyLoss(weight=class_weights, ignore_index=-100)
+        else:
+            self.ce = nn.CrossEntropyLoss(ignore_index=-100)
         self.mse = nn.MSELoss(reduction='none')
         self.num_classes = num_classes
         self.fold = split
         self.weighted = weighted
         self.weighted_str = "Weighted" if self.weighted else "Regular"
+        self.kl = kl
+        self.kl_str = "KL" if self.kl else "No-KL"
+        self.class_weights = class_weights is not None
+        self.class_weights_str = "ClassWeighting" if self.class_weights else "NoClassWeighting"
+        self.exp_name = "-".join([self.weighted_str, self.kl_str, self.class_weights_str])
 
         logger.add('logs/' + dataset + "_" + split + "_{time}.log")
         logger.add(sys.stdout, colorize=True, format="{message}")
@@ -186,9 +194,12 @@ class Trainer:
                     #         min=0, max=16) * mask[:, :, 1:])
                     loss += self.ce(p.transpose(2, 1).contiguous().view(-1, self.num_classes),
                                     batch_target.view(-1))
-                    loss += 0.15 * torch.mean(torch.clamp(
+                    loss += 0.5 * torch.mean(torch.clamp(
                         self.mse(F.log_softmax(p[:, :, 1:], dim=1), F.log_softmax(p.detach()[:, :, :-1], dim=1)),
                         min=0, max=16) * mask[:, :, 1:])
+                    if self.kl:
+                        loss += 0.5 * torch.mean(F.softmax(p[:, :, 1:], dim=1) * (
+                                F.log_softmax(p[:, :, 1:], dim=1) - F.log_softmax(p.detach()[:, :, :-1], dim=1)))
 
                 epoch_loss_train += loss.item()
                 loss.backward()
@@ -200,7 +211,7 @@ class Trainer:
                 tp, fp, fn = np.zeros(3), np.zeros(3), np.zeros(3)
 
                 for s in range(len(overlap)):
-                    tp1, fp1, fn1 = f_score(predicted.squeeze(0).tolist(), batch_target.squeeze(0).tolist(), overlap[s],
+                    tp1, fp1, fn1 = f_score(predicted.view(-1).tolist(), batch_target.view(-1).tolist(), overlap[s],
                                             train=True)
                     tp[s] += tp1
                     fp[s] += fp1
@@ -235,7 +246,7 @@ class Trainer:
                 total_val += torch.sum(mask[:, 0, :]).item()
                 tp, fp, fn = np.zeros(3), np.zeros(3), np.zeros(3)
                 for s in range(len(overlap)):
-                    tp1, fp1, fn1 = f_score(predicted.squeeze(0).tolist(), batch_target.squeeze(0).tolist(), overlap[s],
+                    tp1, fp1, fn1 = f_score(predicted.view(-1).tolist(), batch_target.view(-1).tolist(), overlap[s],
                                             train=True)
                     tp[s] += tp1
                     fp[s] += fp1
@@ -262,25 +273,25 @@ class Trainer:
                 "[epoch %d]: epoch loss validation set = %f,   acc_val = %f" % (
                     epoch + 1, epoch_loss_val / len(batch_gen_val.list_of_examples),
                     float(correct_val) / total_val))
-            clogger.report_scalar(self.weighted_str + " Losses - " + self.fold, "Train Loss", iteration=epoch + 1,
+            clogger.report_scalar(self.exp_name + " Losses - " + self.fold, "Train Loss", iteration=epoch + 1,
                                   value=epoch_loss_train / len(batch_gen_train.list_of_examples))
-            clogger.report_scalar(self.weighted_str + " Losses - " + self.fold, "Validation Loss", iteration=epoch + 1,
+            clogger.report_scalar(self.exp_name + " Losses - " + self.fold, "Validation Loss", iteration=epoch + 1,
                                   value=epoch_loss_val / len(batch_gen_val.list_of_examples))
-            clogger.report_scalar(self.weighted_str + " Accuracies - " + self.fold, "Train Accuracy",
+            clogger.report_scalar(self.exp_name + " Accuracies - " + self.fold, "Train Accuracy",
                                   iteration=epoch + 1,
                                   value=(float(correct_train) / total_train))
-            clogger.report_scalar(self.weighted_str + " Accuracies - " + self.fold, "Validation Accuracy",
+            clogger.report_scalar(self.exp_name + " Accuracies - " + self.fold, "Validation Accuracy",
                                   iteration=epoch + 1,
                                   value=(float(correct_val) / total_val))
             for k in range(len(overlap)):
-                clogger.report_scalar(self.weighted_str + " F1 Validation - " + self.fold,
+                clogger.report_scalar(self.exp_name + " F1 Validation - " + self.fold,
                                       f"F1@{int(overlap[k] * 100)}",
                                       iteration=epoch + 1,
                                       value=(float(f1s_val[k]) / len(batch_gen_val.list_of_examples)))
-                clogger.report_scalar(self.weighted_str + " F1 Train - " + self.fold, f"F1@{int(overlap[k] * 100)}",
+                clogger.report_scalar(self.exp_name + " F1 Train - " + self.fold, f"F1@{int(overlap[k] * 100)}",
                                       iteration=epoch + 1,
                                       value=(float(f1s_train[k]) / len(batch_gen_train.list_of_examples)))
-        logger.info(f"{self.weighted_str} Run: Best Validation accuracy = %f at epoch = %f" % (best_acc, best_epoch))
+        logger.info(f"{self.exp_name} Run: Best Validation accuracy = %f at epoch = %f" % (best_acc, best_epoch))
 
     def predict(self, model_dir, results_dir, features_path, vid_list_file, epoch, actions_dict, device, sample_rate):
         self.model.eval()